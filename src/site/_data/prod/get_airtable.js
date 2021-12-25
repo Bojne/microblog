2f040fb13c86bab2f8878da64278a133aa123a35
bojne <bojne.john@gmail.com>
2021-12-25 22:03:13 +0100

    modify airtable export

--- src/site/_data/prod/get_airtable.js
+++ src/site/_data/prod/get_airtable.js
@@ -1,53 +1,55 @@
-const seed = require("../../../utils/save-seed.js");
-var Airtable = require("airtable");
+module.exports = function () {
+  const seed = require("../../../utils/save-seed.js");
+  var Airtable = require("airtable");
 
-const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-if (!AIRTABLE_API_KEY) {
-  console.error("No Airtable API key found!");
-}
+  const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+  if (!AIRTABLE_API_KEY) {
+    console.error("No Airtable API key found!");
+  }
 
-Airtable.configure({
-  endpointUrl: "https://api.airtable.com",
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
+  Airtable.configure({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.AIRTABLE_API_KEY,
+  });
 
-// based on the base(table) id
-var base = Airtable.base("appcKPyNrw9c9mtYV");
-let data = {};
+  // based on the base(table) id
+  var base = Airtable.base("appcKPyNrw9c9mtYV");
+  let data = {};
 
-base("content")
-  .select({
-    // Selecting the first 5 records in Grid view:
-    maxRecords: 99,
-    view: "Grid view",
-    cellFormat: "json",
-  })
-  .eachPage(
-    function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
+  base("content")
+    .select({
+      // Selecting the first 5 records in Grid view:
+      maxRecords: 99,
+      view: "Grid view",
+      cellFormat: "json",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
 
-      records.forEach(function (record) {
-        let type = record.get("type");
-        if (!(type in data)) {
-          data[type] = [];
-        }
-        data[type].push({
-          time: record.get("time"),
-          message: record.get("message"),
+        records.forEach(function (record) {
+          let type = record.get("type");
+          if (!(type in data)) {
+            data[type] = [];
+          }
+          data[type].push({
+            time: record.get("time"),
+            message: record.get("message"),
+          });
+          // console.log("Retrieved", record.get("type"));
         });
-        // console.log("Retrieved", record.get("type"));
-      });
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
-    },
-    function done(err) {
-      if (err) {
-        console.error(err);
-        return;
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`Loading data... 😍`);
+        seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
       }
-      console.log(`Loading data... 😍`);
-      seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
-    }
-  );
+    );
+};
