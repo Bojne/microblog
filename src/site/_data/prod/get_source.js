af3cbe26d98686cb2a43e09bd712509ca43a213c
bojne <bojne.john@gmail.com>
2022-02-20 03:18:21 +0100

    change from service account to api key

--- src/site/_data/prod/get_source.js
+++ src/site/_data/prod/get_source.js
@@ -1,20 +1,24 @@
 const seed = require("../../../utils/save-seed.js");
 const { google } = require("googleapis");
 
-async function asyncCall() {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+async function asyncGetData() {
+  const api_key = process.env.SHEET_API_KEY;
+  const range = process.env.RANGE;
+
+  if (!api_key || !range) {
+    console.error("Don't have a valid google api key in envrionment");
+  }
+  const sheets = google.sheets({
+    version: "v4",
+    auth: api_key,
   });
-  const range = `Sheet1!A1:D1000`; // #TODO: Made this more dynamic
-  const sheets = google.sheets({ version: "v4", auth });
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
     range,
   });
   data = {};
   response.data.values.forEach(([type, time, id, text]) => {
-    if (id == "900136250" && text) {
-      //#TODO: make the filter a global level
+    if (id == process.env.MY_TG_ID && text) {
       if (!(type in data)) {
         data[type] = [];
       }
@@ -22,8 +26,7 @@ async function asyncCall() {
       data["date"];
     }
   });
-
   seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
 }
 
-asyncCall();
+asyncGetData();
